7dc44f5ee9c891a0915c1d39a655e7a60a09ea91
Juanco <juanffo874@gmail.com>
2024-10-07 18:44:55 -0600

    Comenta las funciones que aun no han sido creadas para evitar errores en el desarrollo

--- routes/userRoutes.routes.js
+++ routes/userRoutes.routes.js
@@ -5,8 +5,8 @@ import { createUser } from "../controller/userController.js";
 const router = Router();
 
 router.post('/createUser', createUser)
-router.post('/authenticate', authenticateUser)
-router.get('/confirm/:token', confirmToken)
-router.get('/forgotPassword/:token', forgotPassword)
+// router.post('/authenticate', authenticateUser)
+// router.get('/confirm/:token', confirmToken)
+// router.get('/forgotPassword/:token', forgotPassword)
 
 export default router
