15f5f73e747a7ea0484b1b046b2381ca09e5e490
Juanco <juanffo874@gmail.com>
2024-10-11 17:28:28 -0600

    Añade el puerto a la configuración de la base de datos

--- database/db.js
+++ database/db.js
@@ -5,6 +5,7 @@ dotenv.config({path: '.env'})
 
 const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, {
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: 'mysql', // tipo de motor de base de datos
     //... otros parámetros de la conexión
     pool: {
