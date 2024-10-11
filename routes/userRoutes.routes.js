678a2829d225330f3e3de5f831c953a22c2f014e
Juanco <juanffo874@gmail.com>
2024-10-10 20:48:43 -0600

    Confirma al usuario y habilita su ruta

--- routes/userRoutes.routes.js
+++ routes/userRoutes.routes.js
@@ -1,12 +1,12 @@
 //Define las rutas para el usuario y autenticacion
 import { Router } from "express";
-import { createUser } from "../controller/userController.js";
+import { createUser, confirmToken } from "../controller/userController.js";
 
 const router = Router();
 
 router.post('/createUser', createUser)
+router.get('/confirm/:token', confirmToken)
 // router.post('/authenticate', authenticateUser)
-// router.get('/confirm/:token', confirmToken)
 // router.get('/forgotPassword/:token', forgotPassword)
 
 export default router
